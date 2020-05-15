f071f7bd1a249d1a0f3052b888152172f03c737b
AdelNabli <adel.nabli91@orange.fr>
2020-05-15 02:29:35 -0400

    add batches

--- MCN/MCN_curriculum/train_baselines.py
+++ MCN/MCN_curriculum/train_baselines.py
@@ -188,7 +188,7 @@ def train_value_net_baseline(batch_size, size_test_data, lr, betas, n_episode, u
 
         # perform an epoch over the replay memory
         # if there is enough new instances in memory
-        if count_instances % n_instance_before_epoch > count_epochs and count_memory > batch_size:
+        if count_instances // n_instance_before_epoch > count_epochs and count_memory > batch_size:
             count_epochs += 1
             # create a list of randomly shuffled indices to sample batches from
             memory_size = len(replay_memory)
