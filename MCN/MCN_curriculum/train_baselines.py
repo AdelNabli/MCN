810af9833681f62b3baad2eae7e221f0372d75b0
AdelNabli <adel.nabli91@orange.fr>
2020-05-18 00:27:52 -0400

    add batches

--- MCN/MCN_curriculum/train_baselines.py
+++ MCN/MCN_curriculum/train_baselines.py
@@ -263,15 +263,6 @@ def train_value_net_baseline(batch_size, size_test_data, lr, betas, n_episode, u
                         " \n Losses on test set : ", losses_test,
                     )
 
-        # add the instances from the episode to memory
-        for k in range(len(instances_episode)):
-            instance = instances_episode[k]
-            instance.target = torch.tensor([value[k % size_batch_instances]], dtype=torch.float).view([1, 1]).to(device)
-            if len(replay_memory) < size_memory:
-                replay_memory.append(None)
-            replay_memory[count_memory % size_memory] = instance
-            count_memory += 1
-
     # Compute how the neural networks we trained perform on the test set
     if size_test_data > 0:
         if path_test_data is None:
